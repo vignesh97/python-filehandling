42d9abed6bae5b05d5ca77afd40020d3d23548e0
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-01-07 18:33:26 -0500

    finding len using for loop

--- read_file.py
+++ read_file.py
@@ -1,7 +1,12 @@
 file= open("example.txt",'r')
-value=file.readline()
+'''value=file.readline()
 while len(value)!=0:
-    print(len(value))
-    value=file.readline()
+    print(len(value)-1)
+    value=file.readline()'''
+
+content  = file.readlines()
 file.close()
+for i in content:
+    print(len(i)-1)
+
 #print(content)
