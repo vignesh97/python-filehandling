24604d1d920a653e4deca05061c98341f2e1cfa2
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-01-07 18:45:57 -0500

    append using a

--- writing_file.py
+++ writing_file.py
@@ -1,3 +1,11 @@
+
+numbers = [1, 2, 3]
+file = open("numbers.txt", "a")
+for i in numbers:
+     file.write(str(i) + "\n")
+file.close()
+
+
 file=open("test.txt",'w')
 file.write("Line 1\n")
 file.write("Line 2\n")
